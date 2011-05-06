d8746321b51f710180937b6b421114c6ced9c567
Cucu Ionut <ionutcristiancucu@gmail.com>
2011-05-06 17:07:33 +0300

    am bagat seed pt pacient/protocol

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,19 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+params = {:pacient => {:nume => 'Georgescu', :prenume=>'Maria', :cnp=>1820912200121, :data_integrarii=> '01/05/2011',
+          :pacient_protocol_attributes => {
+            :data => '01/05/2011', :centru => 'Sibiu', :program => 'hd'}
+}}
+#Pacient.create!(params[:pacient])
+
+
+Pacient.create(:nume => 'Ionescu', :prenume => 'Andrei', :cnp => 21209210009, :data_integrarii => '09/09/1999')
+
+Centru.delete_all
+[{"Georgescu", "Camelia", 2871208100122, 05/05/2011}].each do |pacient|
+  Pacient.create(:pacient)
+end
 
 Centru.delete_all
 
