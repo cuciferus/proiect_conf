45e31d612dde850e129a7d6fd5afcdce19260940
Cucu Ionut <ionutcristiancucu@gmail.com>
2011-06-08 06:52:31 +0300

    inca lucru la alegerea analizei in tabelu show pacient

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def blood_result(object)
+    puts *params
+    remote_function(:url => url_for(object),
+                   :method => :put
+                   )
+  end
+
+
 end
