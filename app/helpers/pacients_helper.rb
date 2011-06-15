67429b8864f2e7acb78bd132ddbdca13d623eefd
Cucu Ionut <ionutcristiancucu@gmail.com>
2011-06-15 19:56:17 +0300

    inca lucru la simple_form

--- app/helpers/pacients_helper.rb
+++ app/helpers/pacients_helper.rb
@@ -1,13 +1,11 @@
 module PacientsHelper
-  def link_to_analize
-    case @pacient.last_protocol.program
-    when 'capd'
-      analiza_path = new_pacient_capd_url(@pacient)
-    when 'hd'
-      analiza_path = new_pacient_hemo_lunar_path(@pacient)
-    when 'brd'
-      analiza_path = new_pacient_bcr_path(@pacient)
-    end
+  def link_to_analize(pacient)
+    analiza_path = case pacient.last_protocol.program
+      when 'capd' then analiza_path = new_pacient_capd_url(pacient)
+      when 'hd' then analiza_path = new_pacient_hemo_lunar_url(pacient)
+      when 'brd' then analiza_path = new_pacient_bcr_path(pacient)
+      end
+
     link_to('Adauga alt set de analize', analiza_path)
   end
 end
